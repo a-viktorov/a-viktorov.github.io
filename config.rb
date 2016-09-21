8d566245e357bbc2f8f0d7468b96c4f601f4a8d9
aLek5Δndr <visualshot@mail.ru>
2016-09-21 20:13:10 +0300

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,8 @@
 # Require any additional compass plugins here.
 
+require 'github/markup'
+GitHub::Markup.render('README.markdown', "* One\n* Two")
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
